76a74c48a7c9da620d0ce4fcabb05cdc96aca056
전상민 <jeonsangmin77@gmail.com>
2020-06-26 19:57:38 +0900

    [Fix] ReportViewController에 ReportHeaderView 추가 & DailyViewController 에서 사용하지 않는 프로퍼티 주석처리

--- Cheers!Calory!/Cheers!Calory!/ViewControllers/Report/ReportViewController.swift
+++ Cheers!Calory!/Cheers!Calory!/ViewControllers/Report/ReportViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ReportViewController: UIViewController {
     
+    private let headerView = ReportHeaderView()
     private let tableView = UITableView()
 
     override func viewDidLoad() {
@@ -18,16 +19,27 @@ class ReportViewController: UIViewController {
     }
     
     private func setUI() {
-        view.addSubview(tableView)
+        
+        [headerView, tableView].forEach {
+            view.addSubview($0)
+        }
+        
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         setConstraint()
     }
     private func setConstraint() {
-        let guide = view.safeAreaLayoutGuide
-        tableView.snp.makeConstraints({
-            $0.leading.trailing.top.bottom.equalTo(guide)
-        })
+       let guide = view.safeAreaLayoutGuide
+       
+       headerView.snp.makeConstraints {
+           $0.top.leading.trailing.equalTo(guide)
+        $0.height.equalToSuperview().multipliedBy(0.4)
+       }
+       
+       tableView.snp.makeConstraints {
+           $0.top.equalTo(headerView.snp.bottom).offset(CGFloat.dynamicYMargin(margin: 15))
+           $0.leading.trailing.bottom.equalTo(guide)
+       }
     }
 }
 
@@ -39,7 +51,6 @@ extension ReportViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let dailyIntake = DailyIntakeDB.shared.getDailyIntake(index: indexPath.row)
-        print(dailyIntake)
         cell.textLabel?.text = dailyIntake?.today
         return cell
     }
