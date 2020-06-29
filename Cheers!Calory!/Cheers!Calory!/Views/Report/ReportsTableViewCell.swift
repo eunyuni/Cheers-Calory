6065d17a9a2b76224ac9342416c64d44dfd594a4
전상민 <jeonsangmin77@gmail.com>
2020-06-29 23:04:46 +0900

    [Fix] 사용하지 않는 프로토콜 삭제

---
+++ Cheers!Calory!/Cheers!Calory!/Views/Report/ReportsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReportsTableViewCell.swift
+//  Cheers!Calory!
+//
+//  Created by MyMac on 2020/06/29.
+//  Copyright © 2020 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class ReportsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
