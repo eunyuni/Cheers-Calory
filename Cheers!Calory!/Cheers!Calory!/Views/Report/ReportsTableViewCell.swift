915bae253ae57582587f1e55eb1c91c1e80b2780
전상민 <jeonsangmin77@gmail.com>
2020-06-29 23:07:22 +0900

    [Add] 리포트 테이블뷰셀 추가 & 사용하지 않는 프로토콜 삭제

--- Cheers!Calory!/Cheers!Calory!/Views/Report/ReportsTableViewCell.swift
+++ Cheers!Calory!/Cheers!Calory!/Views/Report/ReportsTableViewCell.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ReportsTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "reportsCell"
+    
+    let dateLabel = UILabel()
+    let caloryLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        [dateLabel, caloryLabel].forEach {
+            self.addSubview($0)
+        }
+        dateLabel.font = UIFont.dynamicFont(fontSize: 16, weight: .ultraLight)
+        caloryLabel.font = UIFont.dynamicFont(fontSize: 16, weight: .medium)
+        
+        dateLabel.textColor = .gray
+        caloryLabel.textColor = .black
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        dateLabel.snp.makeConstraints {
+            $0.leading.equalTo(self.snp.leading).offset(CGFloat.dynamicXMargin(margin: 20))
+            $0.centerY.equalTo(self.snp.centerY)
+        }
+        
+        caloryLabel.snp.makeConstraints {
+            $0.trailing.equalTo(self.snp.trailing).offset(CGFloat.dynamicXMargin(margin: -24))
+            $0.centerY.equalTo(self.snp.centerY)
+        }
     }
 
 }
