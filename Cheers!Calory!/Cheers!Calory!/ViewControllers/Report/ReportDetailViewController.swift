f82626f4daa1d99028f3367f6e42b7008632cd89
eunyuni <eunyuuni@gmail.com>
2020-07-01 13:04:10 +0900

    [Add] ReportDetailView

---
+++ Cheers!Calory!/Cheers!Calory!/ViewControllers/Report/ReportDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ReportDetailViewController.swift
+//  Cheers!Calory!
+//
+//  Created by 은영김 on 2020/07/01.
+//  Copyright © 2020 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class ReportDetailViewController: UIViewController {
+
+  // MARK: -Property
+  
+  
+  // MARK: -Lift cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setUI()
+  }
+  
+  // MARK: -Action
+  
+  
+  // MARK: -setupUI
+  private func setUI() {
+    
+    setConstraint()
+  }
+  
+  // MARK: -setupConstraint
+  
+  private func setConstraint() {
+    
+  }
+
+}
